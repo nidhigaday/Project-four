f4ccdcd6446b92b899704f7aac9fd89c65a2dcbf
nidhigaday <nidhi.gaday@gmail.com>
2015-08-25 11:32:15 -0400

    correction in Js file

--- js/about.js
+++ js/about.js
@@ -1,19 +1,21 @@
-var bio = {
-    "about": "I am a graduate of Internet Technology with 1 year commercial experience working as technical specialist for web platforms for Online Learning. For the past few years I've been very interesting in Web Designing and since last 1 year I’ve been working on enhancing my responsive web design, front-end development, Bootstrap and Website Optimization skills.",
-    "aboutpic": "Project-four/img/aboutme.png",
-
-    "display": function() {
-        var formattedabout = HTMLabout.replace("%data%", "bio.about");
-        var formattedaboutpic = HTMLavatar.replace("%data%", "bio.aboutpic");
-    }
-};
-
 var HTMLaboutStart = '<div class="about"></div>';
 var HTMLabout = '<p>%data%</p>';
 var HTMLaboutStart = '<div class="avatar"></div>';
 var HTMLavatar = '<img src="%data%">';
 
-$("#aboutme").append(HTMLaboutStart);
-$(".about").append(formattedabout);
-$("#aboutme").append(HTMLaboutStart);
-$(".avatar").append(formattedaboutpic);
+var bio = {
+    "about": "I am a graduate of Internet Technology with 1 year commercial experience working as technical specialist for web platforms for Online Learning. For the past few years I've been very interesting in Web Designing and since last 1 year I’ve been working on enhancing my responsive web design, front-end development, Bootstrap and Website Optimization skills.",
+    "aboutpic": "Project-four/img/aboutme.png",
+
+    "display": function() {
+        var formatabout = HTMLabout.replace("%data%", "bio.about");
+        var formataboutpic = HTMLavatar.replace("%data%", "bio.aboutpic");
+
+        $("#aboutme").append(HTMLaboutStart);
+        $(".about").append(formattedabout);
+        $("#aboutme").append(HTMLaboutStart);
+        $(".avatar").append(formattedaboutpic);
+    }
+};
+
+
