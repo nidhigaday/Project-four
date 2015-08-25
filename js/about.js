72678ef84d6865addfe8a548d35b0bc5e03a8f40
nidhigaday <nidhi.gaday@gmail.com>
2015-08-25 12:51:06 -0400

    correction for element

--- js/about.js
+++ js/about.js
@@ -1,19 +1,19 @@
-var HTMLaboutStart = '<div class="about"></div>';
+var HTMLaboutStartA = '<div class="about"></div>';
 var HTMLabout = '<p>%data%</p>';
-var HTMLaboutStart = '<div class="avatar"></div>';
+var HTMLaboutStartB = '<div class="avatar"></div>';
 var HTMLavatar = '<img src="%data%">';
 
 var bio = {
     "about": "I am a graduate of Internet Technology with 1 year commercial experience working as technical specialist for web platforms for Online Learning. For the past few years I've been very interesting in Web Designing and since last 1 year I’ve been working on enhancing my responsive web design, front-end development, Bootstrap and Website Optimization skills.",
-    "aboutpic": "Project-four/img/aboutme.png",
+    "aboutpic": "./img/aboutme.png",
 
     "display": function() {
-        var formatabout = HTMLabout.replace("%data%", "bio.about");
-        var formataboutpic = HTMLavatar.replace("%data%", "bio.aboutpic");
+        var formattedabout = HTMLabout.replace("%data%", bio.about);
+        var formattedaboutpic = HTMLavatar.replace("%data%", bio.aboutpic);
 
-        $("#aboutme").append(HTMLaboutStart);
+        $("#aboutme").append(HTMLaboutStartA);
         $(".about").append(formattedabout);
-        $("#aboutme").append(HTMLaboutStart);
+        $("#aboutme").append(HTMLaboutStartB);
         $(".avatar").append(formattedaboutpic);
     }
 };
